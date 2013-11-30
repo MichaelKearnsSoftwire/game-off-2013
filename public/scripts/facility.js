063e9855ffa462562b73b4a5f309097c0cba99d4
Michael Kearns <mkearns316@gmail.com>
2013-11-30 21:24:41 +0000

    Add public directory (oops)

---
+++ public/scripts/facility.js
@@ -0,0 +1,47 @@
+define('facility', function() {
+    'use strict';
+
+    return function(facilityTemplate) {
+        var timeBuilt = 0;
+        var isPowered = false;
+
+        this.name = facilityTemplate.name;
+        this.landCost = facilityTemplate.landCost;
+
+        this.energyDelta = function() {
+            return isPowered ? this.baseEnergyDelta() : 0;
+        };
+
+        this.baseEnergyDelta = function() {
+            return this.getDeltas().energy;
+        };
+
+        this.pollutionDelta = function() {
+            return isPowered ? this.getDeltas().pollution : 0;
+        };
+
+        this.foodDelta = function() {
+            return isPowered ? this.getDeltas().food : 0;
+        };
+
+        this.getDeltas = function() {
+            return this.isBuilt() ? facilityTemplate.normalDelta : facilityTemplate.buildDelta ;
+        };
+
+        this.isBuilt = function()
+        {
+            return timeBuilt >= facilityTemplate.buildDuration;
+        };
+
+        this.update = function(remainingPower) {
+            if (remainingPower >= -this.baseEnergyDelta()) {
+                timeBuilt++;
+                isPowered = true;
+                return remainingPower + this.baseEnergyDelta();
+            } else {
+                isPowered = false;
+                return remainingPower;
+            }
+        };
+    };
+});
---
+++ public/scripts/facilityList.js
@@ -0,0 +1,50 @@
+define('facilityList', ['underscore', 'availableFacilities',  'facility'],
+    function(_, availableFacilities,  Facility) {
+    'use strict';
+
+    return function() {
+        var facilities = [];
+        var baseEnergyOutput = 5;
+        this.addFacility = function(facilityName, currentTime) {
+            facilities.push([new Facility(availableFacilities[facilityName]), currentTime]);
+        };
+
+        this.removeFacility = function(facility) {
+            var facilityIndex = _.map(facilities, function(x) { return x[0]; }).indexOf(facility);
+            facilities.splice(facilityIndex, 1);
+        };
+
+        this.getFacilityCount = function() {
+            return facilities.length;
+        };
+
+        this.getFacility = function(index) {
+            return facilities[index][0];
+        };
+
+        this.update = function(currentTime, unfloodedLandArea) {
+
+            var grossEnergyProduced = _.reduce(facilities, function(sum, next) {
+                var delta = next[0].energyDelta();
+                var energyProduced = delta > 0 ? delta : 0;
+                return sum + energyProduced;
+            }, baseEnergyOutput);
+
+            _.reduce(facilities, function(energy, next) {
+                return next[0].update(energy);
+            }, grossEnergyProduced);
+
+            var foodDelta =  _.reduce(facilities, function(sum, next) {
+                return sum + next[0].foodDelta();
+            }, 0);
+            var pollutionDelta = _.reduce(facilities, function(sum, next) { return sum + next[0].pollutionDelta(); }, 0);
+
+            var consumedLandArea = _.reduce(this.facilities, function(sum, next) { return sum + next.landCost; }, 0);
+            return {
+                buildableLandArea: unfloodedLandArea - consumedLandArea,
+                pollutionDelta: pollutionDelta,
+                foodDelta: foodDelta
+            };
+        };
+    };
+});
